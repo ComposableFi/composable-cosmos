582d59fbfdf6f316b0e6d6cfbb16e087aa75428d
ducnt131 <ntd1312000@gmail.com>
2023-04-01 21:03:02 +0700

    Rename module in go.mod

--- cmd/polytoped/main.go
+++ cmd/polytoped/main.go
@@ -5,9 +5,9 @@ import (
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 
-	"github.com/notional-labs/polytope/v2/app"
-	cmd "github.com/notional-labs/polytope/v2/cmd/polytoped/cmd"
-	cmdcfg "github.com/notional-labs/polytope/v2/cmd/polytoped/config"
+	"github.com/notional-labs/composable-testnet/v2/app"
+	cmd "github.com/notional-labs/composable-testnet/v2/cmd/polytoped/cmd"
+	cmdcfg "github.com/notional-labs/composable-testnet/v2/cmd/polytoped/config"
 )
 
 func main() {
