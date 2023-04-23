9ade85f99b02965f2a63c6ee83479553c6d44190
Lặc <expertdicer@gmail.com>
2023-04-23 07:03:16 +0700

    refactor relay test

--- x/transfermiddleware/relay_test.go
+++ x/transfermiddleware/relay_test.go
@@ -1,6 +1,7 @@
 package transfermiddleware_test
 
 import (
+	"fmt"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -20,50 +21,68 @@ func TestFromIBCTransferToContract(t *testing.T) {
 	//           that was started on chain A via ibc transfer module
 
 	transferAmount := sdk.NewInt(1)
-	specs := map[string]struct {
-		expChainABalanceDiff sdk.Int
-		expChainBBalanceDiff sdk.Int
+	var (
+		coordinator = customibctesting.NewCoordinator(t, 2)
+		chainA      = coordinator.GetChain(customibctesting.GetChainID(0))
+		chainB      = coordinator.GetChain(customibctesting.GetChainID(1))
+	)
+	coordinator.CommitBlock(chainA, chainB)
+
+	path := customibctesting.NewPath(chainA, chainB)
+	path.EndpointA.ChannelConfig = &ibctesting.ChannelConfig{
+		PortID:  "transfer",
+		Version: ibctransfertypes.Version,
+		Order:   channeltypes.UNORDERED,
+	}
+	path.EndpointB.ChannelConfig = &ibctesting.ChannelConfig{
+		PortID:  "transfer",
+		Version: ibctransfertypes.Version,
+		Order:   channeltypes.UNORDERED,
+	}
+
+	coordinator.SetupConnections(path)
+	coordinator.CreateChannels(path)
+
+	originalChainABalance := chainA.AllBalances(chainA.SenderAccount.GetAddress())
+	// chainB.SenderAccount: 10000000000000000000stake
+	originalChainBBalance := chainB.AllBalances(chainB.SenderAccount.GetAddress())
+
+	// when transfer via sdk transfer from A (module) -> B (contract)
+	coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, transferAmount)
+	timeoutHeight := clienttypes.NewHeight(1, 110)
+
+	testCases := []struct {
+		name                 string
+		expChainABalanceDiff sdk.Coin
+		expChainBBalanceDiff sdk.Coin
+		malleate             func()
 	}{
-		"ack": {
-			expChainABalanceDiff: transferAmount.Neg(),
-			expChainBBalanceDiff: transferAmount,
+		// {
+		// 	"Transfer with no pre-set ParachainIBCTokenInfo",
+		// 	sdk.NewCoin(sdk.DefaultBondDenom, transferAmount),
+		// 	ibctransfertypes.GetTransferCoin(path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, coinToSendToB.Denom, transferAmount),
+		// 	func() {},
+		// },
+		{
+			"Transfer with pre-set ParachainIBCTokenInfo",
+			sdk.NewCoin(sdk.DefaultBondDenom, transferAmount),
+			sdk.NewCoin(sdk.DefaultBondDenom, transferAmount),
+			func() {
+				// Add parachain token info
+				chainBtransMiddleware := chainB.TransferMiddleware()
+				err := chainBtransMiddleware.AddParachainIBCTokenInfo(chainB.GetContext(), "ibc/C053D637CCA2A2BA030E2C5EE1B28A16F71CCB0E45E8BE52766DC1B241B77878", "channel-0", sdk.DefaultBondDenom)
+				require.NoError(t, err)
+			},
 		},
 	}
-	for name, spec := range specs {
-		t.Run(name, func(t *testing.T) {
-			var (
-				coordinator = customibctesting.NewCoordinator(t, 2)
-				chainA      = coordinator.GetChain(customibctesting.GetChainID(0))
-				chainB      = coordinator.GetChain(customibctesting.GetChainID(1))
-			)
-			coordinator.CommitBlock(chainA, chainB)
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
 
-			path := customibctesting.NewPath(chainA, chainB)
-			path.EndpointA.ChannelConfig = &ibctesting.ChannelConfig{
-				PortID:  "transfer",
-				Version: ibctransfertypes.Version,
-				Order:   channeltypes.UNORDERED,
-			}
-			path.EndpointB.ChannelConfig = &ibctesting.ChannelConfig{
-				PortID:  "transfer",
-				Version: ibctransfertypes.Version,
-				Order:   channeltypes.UNORDERED,
-			}
-
-			coordinator.SetupConnections(path)
-			coordinator.CreateChannels(path)
-			// Setup chainB
-			chainBtransMiddleware := chainB.TransferMiddleware()
-			err := chainBtransMiddleware.AddParachainIBCTokenInfo(chainB.GetContext(), "ibc/C053D637CCA2A2BA030E2C5EE1B28A16F71CCB0E45E8BE52766DC1B241B77878", "channel-0", sdk.DefaultBondDenom)
-			require.NoError(t, err)
-			// Setup chainA
-			originalChainABalance := chainA.Balance(chainA.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
-			originalChainBBalance := chainB.Balance(chainB.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
-			// when transfer via sdk transfer from A (module) -> B (contract)
-			coinToSendToB := sdk.NewCoin(sdk.DefaultBondDenom, transferAmount)
-			timeoutHeight := clienttypes.NewHeight(1, 110)
+			tc.malleate()
+			fmt.Println("chainB.AllBalances(chainB.SenderAccount.GetAddress())", chainB.AllBalances(chainB.SenderAccount.GetAddress()))
 			msg := ibctransfertypes.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, coinToSendToB, chainA.SenderAccount.GetAddress().String(), chainB.SenderAccount.GetAddress().String(), timeoutHeight, 0, "")
-			_, err = chainA.SendMsgs(msg)
+			_, err := chainA.SendMsgs(msg)
 			require.NoError(t, err)
 			require.NoError(t, path.EndpointB.UpdateClient())
 
@@ -80,12 +99,12 @@ func TestFromIBCTransferToContract(t *testing.T) {
 			require.Equal(t, 0, len(chainB.PendingSendPackets))
 
 			// and source chain balance was decreased
-			newChainABalance := chainA.Balance(chainA.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
-			assert.Equal(t, originalChainABalance.Amount.Add(spec.expChainABalanceDiff), newChainABalance.Amount)
+			newChainABalance := chainA.AllBalances(chainA.SenderAccount.GetAddress())
+			assert.Equal(t, originalChainABalance.Sub(tc.expChainABalanceDiff), newChainABalance)
 
 			// and dest chain balance contains voucher
-			expBalance := originalChainBBalance.Add(sdk.NewCoin(sdk.DefaultBondDenom, transferAmount))
-			gotBalance := chainB.Balance(chainB.SenderAccount.GetAddress(), sdk.DefaultBondDenom)
+			expBalance := originalChainBBalance.Add(tc.expChainBBalanceDiff)
+			gotBalance := chainB.AllBalances(chainB.SenderAccount.GetAddress())
 			assert.Equal(t, expBalance, gotBalance)
 		})
 	}
