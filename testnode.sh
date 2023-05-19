b0c9530b63bfb41babe8965d1e980701075c0209
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2023-05-19 14:27:52 -0300

    docker: running devnet with wasm client

--- testnode.sh
+++ testnode.sh
@@ -14,25 +14,25 @@ command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https
 # remove existing daemon
 rm -rf ~/.banksy*
 
-banksyd config keyring-backend $KEYRING
-banksyd config chain-id $CHAINID
+./banksyd config keyring-backend $KEYRING
+./banksyd config chain-id $CHAINID
 
 # if $KEY exists it should be deleted
-echo "taste shoot adapt slow truly grape gift need suggest midnight burger horn whisper hat vast aspect exit scorpion jewel axis great area awful blind" | banksyd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
+echo "taste shoot adapt slow truly grape gift need suggest midnight burger horn whisper hat vast aspect exit scorpion jewel axis great area awful blind" | ./banksyd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
 
-banksyd init $MONIKER --chain-id $CHAINID 
+./banksyd init $MONIKER --chain-id $CHAINID 
 
 # Allocate genesis accounts (cosmos formatted addresses)
-banksyd add-genesis-account $KEY 10000000000upicax --keyring-backend $KEYRING
-banksyd add-genesis-account banksy1594tdya20hxz7kjenkn5w09jljyvdfk8kx5rd6 1000000000000000upicax --keyring-backend $KEYRING
+./banksyd add-genesis-account $KEY 10000000000stakep --keyring-backend $KEYRING
+./banksyd add-genesis-account banksy1594tdya20hxz7kjenkn5w09jljyvdfk8kx5rd6 1000000000000000stake --keyring-backend $KEYRING
 # Sign genesis transaction banksy1594tdya20hxz7kjenkn5w09jljyvdfk8kx5rd6
-banksyd gentx $KEY 10000000000upicax --keyring-backend $KEYRING --chain-id $CHAINID
+./banksyd gentx $KEY 10000000000stake --keyring-backend $KEYRING --chain-id $CHAINID
 
 # Collect genesis tx
-banksyd collect-gentxs
+./banksyd collect-gentxs
 
 # Run this to ensure everything worked and that the genesis file is setup correctly
-banksyd validate-genesis
+./banksyd validate-genesis
 
 if [[ $1 == "pending" ]]; then
   echo "pending mode is on, please wait for the first block committed."
@@ -44,4 +44,5 @@ sed -i'' -e 's/max_body_bytes = /max_body_bytes = 1/g' ~/.banksy/config/config.t
 cat $HOME/.banksy/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="45s"' > $HOME/.banksy/config/tmp_genesis.json && mv $HOME/.banksy/config/tmp_genesis.json $HOME/.banksy/config/genesis.json
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-# banksyd start --pruning=nothing  --minimum-gas-prices=0.0001stake 
+# ./banksyd start --pruning=nothing  --minimum-gas-prices=0.0001stake 
+./banksyd tx 08-wasm push-wasm contracts/ics10_grandpa_cw.wasm --from mykey --keyring-backend test --gas 902152622 --fees 920166stake -y
--- upload_contracts.sh
+++ upload_contracts.sh
@@ -1,1 +1,21 @@
-banksyd tx 08-wasm push-wasm contracts/ics10_grandpa_cw.wasm --from mykey --keyring-backend test --gas 902152622 --fees 920166stake -y
+#!/bin/bash
+
+KEY="mykey"
+CHAINID="banksy-testnet-1"
+KEYALGO="secp256k1"
+KEYRING="test"
+
+# validate dependencies are installed
+command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+
+# remove existing daemon
+rm -rf ~/.banksy*
+
+./banksyd config keyring-backend $KEYRING
+./banksyd config chain-id $CHAINID
+
+# if $KEY exists it should be deleted
+echo "taste shoot adapt slow truly grape gift need suggest midnight burger horn whisper hat vast aspect exit scorpion jewel axis great area awful blind" | ./banksyd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
+
+
+./banksyd tx 08-wasm push-wasm contracts/ics10_grandpa_cw.wasm --from mykey --keyring-backend test --gas 902152622 --fees 920166stake -y
