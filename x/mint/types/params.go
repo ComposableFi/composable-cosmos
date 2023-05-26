92956188840a472f300638705d0da68a9cf94cbe
ducnt131 <ntd1312000@gmail.com>
2023-05-26 16:12:05 +0700

    math.Int

--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -7,6 +7,7 @@ import (
 
 	"sigs.k8s.io/yaml"
 
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
@@ -28,7 +29,7 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 func NewParams(
-	mintDenom string, inflationRateChange, _, _, goalBonded sdk.Dec, blocksPerYear uint64, tokenPerYear sdk.Int,
+	mintDenom string, inflationRateChange, _, _, goalBonded sdk.Dec, blocksPerYear uint64, tokenPerYear math.Int,
 ) Params {
 	return Params{
 		MintDenom:           mintDenom,
