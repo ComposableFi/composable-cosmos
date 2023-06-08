ead357dfe0ceadc5eff96019a1331b9681674895
vuong177 <nguyenvuong1122000@gmail.com>
2023-06-08 16:49:17 +0700

    remove upgrade height in centauri upgrade

--- app/upgrade/centauri/constants.go
+++ app/upgrade/centauri/constants.go
@@ -3,8 +3,4 @@ package centauri
 const (
 	// UpgradeName defines the on-chain upgrade name for the Centauri upgrade.
 	UpgradeName = "centauri"
-
-	// UpgradeHeight defines the block height at which the Centauri upgrade is
-	// triggered.
-	UpgradeHeight = 10000
 )
