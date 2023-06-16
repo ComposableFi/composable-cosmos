abf3e1f71e09c64d093e826b2a3cb4dcd852adad
GnaD13 <cpt.meoz@gmail.com>
2023-06-16 14:51:31 +0700

    add emit event for tx

---
+++ x/transfermiddleware/types/events.go
@@ -0,0 +1,11 @@
+package types
+
+// staking module event types
+const (
+	EventAddParachainIBCTokenInfo    = "add-parachain-token-info"
+	EventRemoveParachainIBCTokenInfo = "remove-parachain-token-info"
+
+	AttributeKeyNativeDenom = "native-denom"
+	AttributeKeyIbcDenom    = "ibc-denom"
+	AttributeKeyAssetID     = "asset-id"
+)
