4c723d8b4d973febae3ad3b973d86cf8d565c375
anhductn2001 <ntd1312000@gmail.com>
2023-06-16 12:59:44 +0700

    add error message

--- x/transfermiddleware/types/errors.go
+++ x/transfermiddleware/types/errors.go
@@ -8,4 +8,5 @@ var (
 	ErrDuplicateParachainIBCTokenInfo = sdkerrors.Register(ModuleName, 1, "duplicate ParachainIBC Token Info")
 	InvalidIBCDenom                   = sdkerrors.Register(ModuleName, 2, "invalid ibc denom")
 	NotFungibleTokenPacketData        = sdkerrors.Register(ModuleName, 3, "not fungible token packet data")
+	NotRegisteredNativeDenom          = sdkerrors.Register(ModuleName, 4, "nativeDenom is not registered")
 )
