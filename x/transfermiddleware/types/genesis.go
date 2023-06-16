f739bf04cee7bcfa32de7d6ef48e961183c5cc6e
Lặc <expertdicer@gmail.com>
2023-06-16 13:40:46 +0700

    validate genesis for transfer middleware

--- x/transfermiddleware/types/genesis.go
+++ x/transfermiddleware/types/genesis.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	fmt "fmt"
+)
+
 // DefaultGenesisState returns a GenesisState with "transfer" as the default PortID.
 func DefaultGenesisState() *GenesisState {
 	return &GenesisState{}
@@ -7,6 +11,34 @@ func DefaultGenesisState() *GenesisState {
 
 // ValidateGenesis validates the provided genesis state to ensure the
 // expected invariants holds.
-func ValidateGenesis(_ GenesisState) error {
+func ValidateGenesis(data GenesisState) error {
+	if err := validateTokenInfos(data.TokenInfos); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateTokenInfos(infos []ParachainIBCTokenInfo) error {
+	infoMap := make(map[string]bool, len(infos))
+
+	for i := 0; i < len(infos); i++ {
+		info := infos[i]
+
+		err := info.ValidateBasic()
+		if err != nil {
+			return err
+		}
+
+		strKey := info.AssetId
+
+		// check duplicate based on assetId
+		if _, ok := infoMap[strKey]; ok {
+			return fmt.Errorf("duplicate parachain token info in genesis state: assetId %v, nativeDenom %v", info.AssetId, info.NativeDenom)
+		}
+
+		infoMap[strKey] = true
+	}
+
 	return nil
 }
