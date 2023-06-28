456eb6a6f1aa5d99627ab21f2086afad779743c1
ducnt131 <ntd1312000@gmail.com>
2023-06-28 11:02:01 +0700

    upgrade reward

--- tests/interchaintest/upgrade_chain_test.go
+++ tests/interchaintest/upgrade_chain_test.go
@@ -20,7 +20,7 @@ const (
 
 func TestCentauriUpgrade(t *testing.T) {
 	repo, version := GetDockerImageInfo()
-	CosmosChainUpgradeTest(t, repo, version, "test")
+	CosmosChainUpgradeTest(t, repo, version, "reward")
 }
 
 func CosmosChainUpgradeTest(t *testing.T, upgradeContainerRepo, upgradeVersion, upgradeName string) {
@@ -39,7 +39,7 @@ func CosmosChainUpgradeTest(t *testing.T, upgradeContainerRepo, upgradeVersion, 
 				Images: []ibc.DockerImage{
 					{
 						Repository: "ghcr.io/notional-labs/centauri",
-						Version:    "3.0.2",
+						Version:    "3.1.2",
 						UidGid:     "1025:1025",
 					},
 				},
