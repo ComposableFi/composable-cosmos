8f78ea0a30898f3d4bc53f8134bd5ffe30e43082
GNaD13 <89174180+GNaD13@users.noreply.github.com>
2023-07-19 14:20:05 +0700

    Add waiting time to remove parachain token (#201)
    * param
    
    * add app.go
    
    * nit
    
    * refactor proto
    
    * add store remove list
    
    * add abci
    
    * correct time

--- app/keepers/keepers.go
+++ app/keepers/keepers.go
@@ -239,6 +239,7 @@ func (appKeepers *AppKeepers) InitNormalKeepers(
 
 	appKeepers.TransferMiddlewareKeeper = transfermiddlewarekeeper.NewKeeper(
 		appKeepers.keys[transfermiddlewaretypes.StoreKey],
+		appKeepers.GetSubspace(transfermiddlewaretypes.ModuleName),
 		appCodec,
 		&appKeepers.HooksICS4Wrapper,
 		&appKeepers.TransferKeeper,
@@ -413,6 +414,7 @@ func (appKeepers *AppKeepers) initParamsKeeper(appCodec codec.BinaryCodec, legac
 	paramsKeeper.Subspace(ibchost.ModuleName)
 	paramsKeeper.Subspace(alliancemoduletypes.ModuleName)
 	paramsKeeper.Subspace(wasm.ModuleName)
+	paramsKeeper.Subspace(transfermiddlewaretypes.ModuleName)
 
 	return paramsKeeper
 }
---
+++ x/transfermiddleware/keeper/abci.go
@@ -0,0 +1,20 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/notional-labs/centauri/v3/x/transfermiddleware/types"
+)
+
+// BeginBlocker of epochs module.
+func (k Keeper) BeginBlocker(ctx sdk.Context) {
+	// Iterate over remove list
+	k.IterateRemoveListInfo(ctx, func(removeList types.RemoveParachainIBCTokenInfo) (stop bool) {
+		// If pass the duration, remove parachain token info
+		if ctx.BlockTime().After(removeList.RemoveTime) {
+			k.RemoveParachainIBCInfo(ctx, removeList.NativeDenom)
+		}
+
+		return false
+	})
+
+}
