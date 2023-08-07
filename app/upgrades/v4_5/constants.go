a02aac0933834e9943f8971934d6aa768803073f
vuong177 <nguyenvuong1122000@gmail.com>
2023-08-07 17:27:05 +0700

    update fork height to 967554

--- app/upgrades/v4_5/constants.go
+++ app/upgrades/v4_5/constants.go
@@ -5,7 +5,7 @@ import "github.com/notional-labs/centauri/v4/app/upgrades"
 const (
 	// UpgradeName defines the on-chain upgrade name for the Centauri upgrade.
 	UpgradeName   = "v4_5"
-	UpgradeHeight = 957554
+	UpgradeHeight = 967554
 )
 
 var Fork = upgrades.Fork{
