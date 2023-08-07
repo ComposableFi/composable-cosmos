42b58cc558aa67eb2cf4a5389928a60f649258f1
vuong177 <nguyenvuong1122000@gmail.com>
2023-08-07 16:11:39 +0700

    add v4_5 fork upgrade

---
+++ app/upgrades/v4_5/constants.go
@@ -0,0 +1,15 @@
+package v45
+
+import "github.com/notional-labs/centauri/v4/app/upgrades"
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the Centauri upgrade.
+	UpgradeName   = "v4_5"
+	UpgradeHeight = 957554
+)
+
+var Fork = upgrades.Fork{
+	UpgradeName:    UpgradeName,
+	UpgradeHeight:  UpgradeHeight,
+	BeginForkLogic: RunForkLogic,
+}
