0535f8af8725aaec571e9f8af07221dce8338809
Lặc <expertdicer@gmail.com>
2023-08-22 17:59:44 +0700

    make proto gen

--- x/tx-boundary/types/boundary.pb.go
+++ x/tx-boundary/types/boundary.pb.go
@@ -27,8 +27,8 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Boundary defines the number of tx limit and block per tx generation time
 type Boundary struct {
-	Boundary            uint64 `protobuf:"varint,1,opt,name=boundary,proto3" json:"boundary,omitempty"`
-	BlocksPerGeneration uint64 `protobuf:"varint,2,opt,name=blocks_per_generation,json=blocksPerGeneration,proto3" json:"blocks_per_generation,omitempty"`
+	TxLimit             uint64 `protobuf:"varint,1,opt,name=tx_limit,json=txLimit,proto3" json:"tx_limit,omitempty"`
+	BlocksPerGeneration int64  `protobuf:"varint,2,opt,name=blocks_per_generation,json=blocksPerGeneration,proto3" json:"blocks_per_generation,omitempty"`
 }
 
 func (m *Boundary) Reset()         { *m = Boundary{} }
@@ -64,22 +64,84 @@ func (m *Boundary) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Boundary proto.InternalMessageInfo
 
-func (m *Boundary) GetBoundary() uint64 {
+func (m *Boundary) GetTxLimit() uint64 {
 	if m != nil {
-		return m.Boundary
+		return m.TxLimit
 	}
 	return 0
 }
 
-func (m *Boundary) GetBlocksPerGeneration() uint64 {
+func (m *Boundary) GetBlocksPerGeneration() int64 {
 	if m != nil {
 		return m.BlocksPerGeneration
 	}
 	return 0
 }
 
+// Boundary defines the number of delegate and redelegate per Addr
+type LimitPerAddr struct {
+	DelegateCount     uint64 `protobuf:"varint,1,opt,name=delegate_count,json=delegateCount,proto3" json:"delegate_count,omitempty"`
+	ReledegateCount   uint64 `protobuf:"varint,2,opt,name=reledegate_count,json=reledegateCount,proto3" json:"reledegate_count,omitempty"`
+	LatestUpdateBlock int64  `protobuf:"varint,3,opt,name=latest_update_block,json=latestUpdateBlock,proto3" json:"latest_update_block,omitempty"`
+}
+
+func (m *LimitPerAddr) Reset()         { *m = LimitPerAddr{} }
+func (m *LimitPerAddr) String() string { return proto.CompactTextString(m) }
+func (*LimitPerAddr) ProtoMessage()    {}
+func (*LimitPerAddr) Descriptor() ([]byte, []int) {
+	return fileDescriptor_668b8d6584310f2c, []int{1}
+}
+func (m *LimitPerAddr) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *LimitPerAddr) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_LimitPerAddr.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *LimitPerAddr) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LimitPerAddr.Merge(m, src)
+}
+func (m *LimitPerAddr) XXX_Size() int {
+	return m.Size()
+}
+func (m *LimitPerAddr) XXX_DiscardUnknown() {
+	xxx_messageInfo_LimitPerAddr.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LimitPerAddr proto.InternalMessageInfo
+
+func (m *LimitPerAddr) GetDelegateCount() uint64 {
+	if m != nil {
+		return m.DelegateCount
+	}
+	return 0
+}
+
+func (m *LimitPerAddr) GetReledegateCount() uint64 {
+	if m != nil {
+		return m.ReledegateCount
+	}
+	return 0
+}
+
+func (m *LimitPerAddr) GetLatestUpdateBlock() int64 {
+	if m != nil {
+		return m.LatestUpdateBlock
+	}
+	return 0
+}
+
 func init() {
 	proto.RegisterType((*Boundary)(nil), "centauri.txboundary.v1beta1.Boundary")
+	proto.RegisterType((*LimitPerAddr)(nil), "centauri.txboundary.v1beta1.LimitPerAddr")
 }
 
 func init() {
@@ -87,22 +149,27 @@ func init() {
 }
 
 var fileDescriptor_668b8d6584310f2c = []byte{
-	// 226 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xd2, 0x4a, 0x4e, 0xcd, 0x2b,
-	0x49, 0x2c, 0x2d, 0xca, 0xd4, 0x2f, 0xa9, 0x48, 0xca, 0x2f, 0xcd, 0x4b, 0x49, 0x2c, 0xaa, 0xd4,
-	0x2f, 0x33, 0x4c, 0x4a, 0x2d, 0x49, 0x34, 0xd4, 0x87, 0x09, 0xe8, 0x15, 0x14, 0xe5, 0x97, 0xe4,
-	0x0b, 0x49, 0xc3, 0xd4, 0xea, 0x21, 0xd4, 0xea, 0x41, 0xd5, 0x4a, 0x89, 0xa4, 0xe7, 0xa7, 0xe7,
-	0x83, 0xd5, 0xe9, 0x83, 0x58, 0x10, 0x2d, 0x52, 0x72, 0xe9, 0xf9, 0xf9, 0xe9, 0x39, 0xa9, 0xfa,
-	0x60, 0x5e, 0x52, 0x69, 0x9a, 0x7e, 0x4a, 0x69, 0x51, 0x62, 0x49, 0x66, 0x7e, 0x1e, 0x54, 0x5e,
-	0x1e, 0x5d, 0xbe, 0x24, 0x33, 0x37, 0xb5, 0xb8, 0x24, 0x31, 0xb7, 0x00, 0xa2, 0x40, 0x29, 0x8a,
-	0x8b, 0xc3, 0x09, 0x6a, 0x95, 0x90, 0x14, 0x17, 0x07, 0xcc, 0x5a, 0x09, 0x46, 0x05, 0x46, 0x0d,
-	0x96, 0x20, 0x38, 0x5f, 0xc8, 0x88, 0x4b, 0x34, 0x29, 0x27, 0x3f, 0x39, 0xbb, 0x38, 0xbe, 0x20,
-	0xb5, 0x28, 0x3e, 0x3d, 0x35, 0x2f, 0x15, 0x62, 0x8f, 0x04, 0x13, 0x58, 0xa1, 0x30, 0x44, 0x32,
-	0x20, 0xb5, 0xc8, 0x1d, 0x2e, 0xe5, 0xa4, 0x7b, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c,
-	0x0f, 0x1e, 0xc9, 0x31, 0x4e, 0x78, 0x2c, 0xc7, 0x70, 0xe1, 0xb1, 0x1c, 0xc3, 0x8d, 0xc7, 0x72,
-	0x0c, 0x51, 0xc2, 0x15, 0xfa, 0x25, 0x15, 0xba, 0xf0, 0xf0, 0x28, 0xa9, 0x2c, 0x48, 0x2d, 0x4e,
-	0x62, 0x03, 0xbb, 0xc8, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x60, 0xbc, 0x7c, 0x92, 0x33, 0x01,
-	0x00, 0x00,
+	// 318 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x5c, 0x91, 0xcd, 0x4a, 0xf3, 0x40,
+	0x14, 0x86, 0x3b, 0xed, 0xc7, 0x67, 0x19, 0xfc, 0x4d, 0x15, 0x6a, 0x85, 0xb1, 0x14, 0x84, 0x2a,
+	0x34, 0x43, 0xf5, 0x0a, 0xac, 0x0b, 0x37, 0x2e, 0x4a, 0xc1, 0x85, 0x6e, 0xc2, 0xa4, 0x73, 0x0c,
+	0xc1, 0x34, 0x13, 0x26, 0x67, 0x24, 0xbd, 0x8b, 0x5e, 0x96, 0xcb, 0x2e, 0x5d, 0x4a, 0x7b, 0x23,
+	0x92, 0x99, 0xa6, 0x15, 0x77, 0x39, 0xef, 0xf3, 0x1c, 0xde, 0x43, 0x86, 0xde, 0x4c, 0x21, 0x45,
+	0x61, 0x74, 0xcc, 0xb1, 0x08, 0x95, 0x49, 0xa5, 0xd0, 0x73, 0xfe, 0x31, 0x0c, 0x01, 0xc5, 0x90,
+	0x57, 0x81, 0x9f, 0x69, 0x85, 0xca, 0xbb, 0xa8, 0x5c, 0x7f, 0xe7, 0xfa, 0x1b, 0xb7, 0x73, 0x1a,
+	0xa9, 0x48, 0x59, 0x8f, 0x97, 0x5f, 0x6e, 0xa5, 0xc3, 0x22, 0xa5, 0xa2, 0x04, 0xb8, 0x9d, 0x42,
+	0xf3, 0xc6, 0xa5, 0xd1, 0x02, 0x63, 0x95, 0x6e, 0xf8, 0xe5, 0x5f, 0x8e, 0xf1, 0x0c, 0x72, 0x14,
+	0xb3, 0xcc, 0x09, 0xbd, 0x17, 0xda, 0x1c, 0x6d, 0xaa, 0xbc, 0x73, 0xda, 0xc4, 0x22, 0x48, 0xe2,
+	0x59, 0x8c, 0x6d, 0xd2, 0x25, 0xfd, 0x7f, 0x93, 0x3d, 0x2c, 0x9e, 0xca, 0xd1, 0xbb, 0xa5, 0x67,
+	0x61, 0xa2, 0xa6, 0xef, 0x79, 0x90, 0x81, 0x0e, 0x22, 0x48, 0xc1, 0xd5, 0xb4, 0xeb, 0x5d, 0xd2,
+	0x6f, 0x4c, 0x5a, 0x0e, 0x8e, 0x41, 0x3f, 0x6e, 0x51, 0x6f, 0x41, 0xe8, 0xbe, 0xdd, 0x1e, 0x83,
+	0xbe, 0x97, 0x52, 0x7b, 0x57, 0xf4, 0x50, 0x42, 0x02, 0x91, 0x40, 0x08, 0xa6, 0xca, 0xa4, 0x55,
+	0xcb, 0x41, 0x95, 0x3e, 0x94, 0xa1, 0x77, 0x4d, 0x8f, 0x35, 0x24, 0x20, 0x7f, 0x8b, 0x75, 0x2b,
+	0x1e, 0xed, 0x72, 0xa7, 0xfa, 0xb4, 0x95, 0x08, 0x84, 0x1c, 0x03, 0x93, 0xc9, 0xd2, 0xb6, 0x77,
+	0xb4, 0x1b, 0xf6, 0xa8, 0x13, 0x87, 0x9e, 0x2d, 0x19, 0x95, 0x60, 0x34, 0xf8, 0x5c, 0x31, 0xb2,
+	0x5c, 0x31, 0xf2, 0xbd, 0x62, 0x64, 0xb1, 0x66, 0xb5, 0xe5, 0x9a, 0xd5, 0xbe, 0xd6, 0xac, 0xf6,
+	0xda, 0x2a, 0x38, 0x16, 0x83, 0xed, 0x0b, 0xe1, 0x3c, 0x83, 0x3c, 0xfc, 0x6f, 0xff, 0xd1, 0xdd,
+	0x4f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xaa, 0x16, 0x56, 0x93, 0xc5, 0x01, 0x00, 0x00,
 }
 
 func (m *Boundary) Marshal() (dAtA []byte, err error) {
@@ -130,8 +197,46 @@ func (m *Boundary) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x10
 	}
-	if m.Boundary != 0 {
-		i = encodeVarintBoundary(dAtA, i, uint64(m.Boundary))
+	if m.TxLimit != 0 {
+		i = encodeVarintBoundary(dAtA, i, uint64(m.TxLimit))
+		i--
+		dAtA[i] = 0x8
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *LimitPerAddr) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *LimitPerAddr) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *LimitPerAddr) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.LatestUpdateBlock != 0 {
+		i = encodeVarintBoundary(dAtA, i, uint64(m.LatestUpdateBlock))
+		i--
+		dAtA[i] = 0x18
+	}
+	if m.ReledegateCount != 0 {
+		i = encodeVarintBoundary(dAtA, i, uint64(m.ReledegateCount))
+		i--
+		dAtA[i] = 0x10
+	}
+	if m.DelegateCount != 0 {
+		i = encodeVarintBoundary(dAtA, i, uint64(m.DelegateCount))
 		i--
 		dAtA[i] = 0x8
 	}
@@ -155,11 +260,29 @@ func (m *Boundary) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.Boundary != 0 {
-		n += 1 + sovBoundary(uint64(m.Boundary))
+	if m.TxLimit != 0 {
+		n += 1 + sovBoundary(uint64(m.TxLimit))
 	}
 	if m.BlocksPerGeneration != 0 {
 		n += 1 + sovBoundary(uint64(m.BlocksPerGeneration))
+	}
+	return n
+}
+
+func (m *LimitPerAddr) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.DelegateCount != 0 {
+		n += 1 + sovBoundary(uint64(m.DelegateCount))
+	}
+	if m.ReledegateCount != 0 {
+		n += 1 + sovBoundary(uint64(m.ReledegateCount))
+	}
+	if m.LatestUpdateBlock != 0 {
+		n += 1 + sovBoundary(uint64(m.LatestUpdateBlock))
 	}
 	return n
 }
@@ -201,9 +324,9 @@ func (m *Boundary) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Boundary", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field TxLimit", wireType)
 			}
-			m.Boundary = 0
+			m.TxLimit = 0
 			for shift := uint(0); ; shift += 7 {
 				if shift >= 64 {
 					return ErrIntOverflowBoundary
@@ -213,7 +336,7 @@ func (m *Boundary) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.Boundary |= uint64(b&0x7F) << shift
+				m.TxLimit |= uint64(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
@@ -232,7 +355,114 @@ func (m *Boundary) Unmarshal(dAtA []byte) error {
 				}
 				b := dAtA[iNdEx]
 				iNdEx++
-				m.BlocksPerGeneration |= uint64(b&0x7F) << shift
+				m.BlocksPerGeneration |= int64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipBoundary(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthBoundary
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *LimitPerAddr) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowBoundary
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: LimitPerAddr: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: LimitPerAddr: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field DelegateCount", wireType)
+			}
+			m.DelegateCount = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBoundary
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.DelegateCount |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ReledegateCount", wireType)
+			}
+			m.ReledegateCount = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBoundary
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.ReledegateCount |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		case 3:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field LatestUpdateBlock", wireType)
+			}
+			m.LatestUpdateBlock = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowBoundary
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.LatestUpdateBlock |= int64(b&0x7F) << shift
 				if b < 0x80 {
 					break
 				}
