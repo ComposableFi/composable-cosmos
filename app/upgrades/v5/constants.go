df216a7ec32afbd3d3c2816f539e06c73d89c10d
vuong177 <nguyenvuong1122000@gmail.com>
2023-08-25 15:18:02 +0700

    v5 upgrade

---
+++ app/upgrades/v5/constants.go
@@ -0,0 +1,19 @@
+package v5
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/notional-labs/centauri/v4/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the Centauri upgrade.
+	UpgradeName = "v5"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added: []string{},
+	},
+}
