3d784dd483269514f5fb51559a27a573bb251859
GnaD <cpt.meoz@gmail.com>
2023-09-22 01:57:49 +0700

    nit

--- app/upgrades/v5/constants.go
+++ app/upgrades/v5/constants.go
@@ -2,19 +2,20 @@ package v5
 
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
-	ccvconsumertypes "github.com/cosmos/interchain-security/v3/x/ccv/consumer/types"
+
 	"github.com/notional-labs/centauri/v6/app/upgrades"
+	txboundary "github.com/notional-labs/centauri/v6/x/tx-boundary/types"
 )
 
 const (
 	// UpgradeName defines the on-chain upgrade name for the Centauri upgrade.
-	UpgradeName = "v6"
+	UpgradeName = "v5"
 )
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added: []string{ccvconsumertypes.ModuleName},
+		Added: []string{txboundary.ModuleName},
 	},
 }
