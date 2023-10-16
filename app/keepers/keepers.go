ff41c4249f2077f3618df84d2fa11315c5053247
Lặc <expertdicer@gmail.com>
2023-10-16 13:16:19 +0700

    authority address

--- app/keepers/keepers.go
+++ app/keepers/keepers.go
@@ -107,7 +107,7 @@ import (
 
 const (
 	AccountAddressPrefix = "composable"
-	authorityAddress     = "layer10556m38z4x6pqalr9rl5ytf3cff8q46nz002d6" // convert from: centauri10556m38z4x6pqalr9rl5ytf3cff8q46nk85k9m
+	authorityAddress     = "layer1hj5fveer5cjtn4wd6wstzugjfdxzl0xpkwyjq3" // convert from: centauri10556m38z4x6pqalr9rl5ytf3cff8q46nk85k9m
 )
 
 type AppKeepers struct {
