2686feec865cbd1c10001e7357c3bda9648e97f5
vuong <nguyenvuong1122000@gmail.com>
2023-11-21 15:51:21 +0700

    minor

--- x/tx-boundary/types/keys.go
+++ x/tx-boundary/types/keys.go
@@ -14,10 +14,4 @@ var (
 
 	// RedelegateBoundaryKey is the key to use for the keeper store.
 	RedelegateBoundaryKey = []byte{0x01}
-
-	// TxKey is the key to use for the keeper store.
-	TxKey = []byte{0x02}
-
-	// TxCountKey is the key to use for the keeper store.
-	TxCountKey = []byte{0x03}
 )
