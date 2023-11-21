f40472b3d5f9aa16f928d39c76587966438bad20
vuong <nguyenvuong1122000@gmail.com>
2023-11-21 12:49:59 +0700

    change upgrade name

--- app/upgrades/prefix/constants.go
+++ app/upgrades/prefix/constants.go
@@ -2,21 +2,16 @@ package v7
 
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
-	"github.com/cosmos/cosmos-sdk/x/authz"
 	"github.com/notional-labs/composable/v6/app/upgrades"
-	alliancetypes "github.com/terra-money/alliance/x/alliance/types"
 )
 
 const (
 	// UpgradeName defines the on-chain upgrade name for the composable upgrade.
-	UpgradeName = "v7-prefix-change"
+	UpgradeName = "prefix-change"
 )
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
-	StoreUpgrades: store.StoreUpgrades{
-		Added:   []string{authz.ModuleName},
-		Deleted: []string{alliancetypes.ModuleName},
-	},
+	StoreUpgrades:        store.StoreUpgrades{},
 }
