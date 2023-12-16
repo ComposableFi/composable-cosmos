1a79bc5ce2ea4663242849150d74c72de928d114
rustdev <placex.com@gmail.com>
2023-12-16 12:16:53 +0000

    add staking middleware module

---
+++ x/stakingmiddleware/types/keys.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
+// MinterKey is the key to use for the keeper store.
+var (
+	DelegationKey = []byte{0x01} // key for a delegation
+)
+
+const (
+	// module name
+	ModuleName = "mint"
+
+	// StoreKey is the default store key for mint
+	StoreKey = ModuleName
+)
+
+// GetDelegationKey creates the key for delegator bond with validator
+// VALUE: staking/Delegation
+func GetDelegationKey(delAddr sdk.AccAddress, valAddr sdk.ValAddress) []byte {
+	return append(GetDelegationsKey(delAddr), address.MustLengthPrefix(valAddr)...)
+}
+
+func GetDelegationsKey(delAddr sdk.AccAddress) []byte {
+	return append(DelegationKey, address.MustLengthPrefix(delAddr)...)
+}
