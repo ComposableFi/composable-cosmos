07200755cef4ae71bf586b8eafc96acd68608b30
Jacob Gadikian <jacobgadikian@gmail.com>
2023-12-21 04:34:09 +0700

    small updates

--- scripts/compatiblity.bash
+++ scripts/compatiblity.bash
@@ -10,5 +10,4 @@ mv genesis.json  ~/.banksy/config/genesis.json
 mv composable_2959777.tar.lz4 ~/.banksy/composable_2959777.tar.lz4
 cd ~/.banksy
 lz4 -d composable_2959777.tar.lz4 | tar -xvf -
-rm composable_2959777.tar.lz4
 centaurid start
