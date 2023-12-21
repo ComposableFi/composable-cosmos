1af77d8928e8b772062bba6899cdcd644137e96c
rustdev <placex.com@gmail.com>
2023-12-21 21:07:02 +0000

    compete EndBlock for custom staking module. batch bond/unbond/redelegate

--- custom/staking/module.go
+++ custom/staking/module.go
@@ -61,77 +61,71 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 func (am AppModule) EndBlock(ctx sdk.Context, _abc abcitype.RequestEndBlock) []abcitype.ValidatorUpdate {
 
 	println("EndBlock Custom Staking Module")
+	params := am.keeper.Stakingmiddleware.GetParams(ctx)
+	println("BlocksPerEpoch: ", params.BlocksPerEpoch)
+	println("Height: ", _abc.Height)
 
-	delegations := am.keeper.Stakingmiddleware.DequeueAllDelegation(ctx)
-	println("Delegations: ", delegations)
-	println("Delegations len: ", len(delegations))
-	//for delegations print the delegator address and the validator address
-	for _, delegation := range delegations {
-		println("Delegator Address: ", delegation.DelegatorAddress)
-		println("Validator Address: ", delegation.ValidatorAddress)
-		fmt.Println("Amount", delegation.Amount.Amount)
+	should_execute_batch := (_abc.Height % int64(params.BlocksPerEpoch)) == 0
+	if should_execute_batch {
+		println("Should batch delegation to be executed at block: ", _abc.Height)
 
-		msgDelegate := stakingtypes.MsgDelegate{DelegatorAddress: delegation.DelegatorAddress, ValidatorAddress: delegation.ValidatorAddress, Amount: delegation.Amount}
-		_, err := am.msgServer.Delegate(ctx, &msgDelegate)
-		if err != nil {
-			println("Error for Delegator Address: ", delegation.DelegatorAddress)
+		delegations := am.keeper.Stakingmiddleware.DequeueAllDelegation(ctx)
+		println("Delegations: ", delegations)
+		println("Delegations len: ", len(delegations))
+		for _, delegation := range delegations {
+			println("Delegator Address: ", delegation.DelegatorAddress)
+			println("Validator Address: ", delegation.ValidatorAddress)
+			fmt.Println("Amount", delegation.Amount.Amount)
+
+			msgDelegate := stakingtypes.MsgDelegate{DelegatorAddress: delegation.DelegatorAddress, ValidatorAddress: delegation.ValidatorAddress, Amount: delegation.Amount}
+			_, err := am.msgServer.Delegate(ctx, &msgDelegate)
+			if err != nil {
+				println("Error for Delegator Address: ", delegation.DelegatorAddress)
+			}
+		}
+
+		beginredelegations := am.keeper.Stakingmiddleware.DequeueAllRedelegation(ctx)
+		println("BeginRedelegations: ", beginredelegations)
+		println("BeginRedelegations len: ", len(beginredelegations))
+		for _, redelegation := range beginredelegations {
+			println("Delegator Address: ", redelegation.DelegatorAddress)
+			println("Validator Address: ", redelegation.ValidatorSrcAddress)
+
+			msg_redelegation := stakingtypes.MsgBeginRedelegate{DelegatorAddress: redelegation.DelegatorAddress, ValidatorSrcAddress: redelegation.ValidatorSrcAddress, ValidatorDstAddress: redelegation.ValidatorDstAddress, Amount: redelegation.Amount}
+			_, err := am.msgServer.BeginRedelegate(ctx, &msg_redelegation)
+			if err != nil {
+				println("Error for Delegator Address: ", msg_redelegation.DelegatorAddress)
+			}
+		}
+
+		undelegations := am.keeper.Stakingmiddleware.DequeueAllUndelegation(ctx)
+		println("Undelegation: ", beginredelegations)
+		println("Undelegation len: ", len(beginredelegations))
+		for _, undelegation := range undelegations {
+			println("Undelegation Delegator Address: ", undelegation.DelegatorAddress)
+			println("Undelegation Validator Address: ", undelegation.ValidatorAddress)
+
+			msg_undelegate := stakingtypes.MsgUndelegate{DelegatorAddress: undelegation.DelegatorAddress, ValidatorAddress: undelegation.ValidatorAddress, Amount: undelegation.Amount}
+			_, err := am.msgServer.Undelegate(ctx, &msg_undelegate)
+			if err != nil {
+				println("Error for Delegator Address: ", msg_undelegate.DelegatorAddress)
+			}
+		}
+
+		cancel_unbonding_delegations := am.keeper.Stakingmiddleware.DequeueAllCancelUnbondingDelegation(ctx)
+		println("Cancel Unbonding Delegations: ", cancel_unbonding_delegations)
+		println("Cancel Ubonding Delegations len: ", len(cancel_unbonding_delegations))
+		for _, cancel_unbonding_delegation := range cancel_unbonding_delegations {
+			println("Cancel Unbonding Delegation  Delegator Address: ", cancel_unbonding_delegation.DelegatorAddress)
+			println("Cancel Unbonding Delegations Validator Address: ", cancel_unbonding_delegation.ValidatorAddress)
+
+			msg_cancle_unbonding_delegation := stakingtypes.MsgCancelUnbondingDelegation{DelegatorAddress: cancel_unbonding_delegation.DelegatorAddress, ValidatorAddress: cancel_unbonding_delegation.ValidatorAddress, Amount: cancel_unbonding_delegation.Amount, CreationHeight: cancel_unbonding_delegation.CreationHeight}
+			_, err := am.msgServer.CancelUnbondingDelegation(ctx, &msg_cancle_unbonding_delegation)
+			if err != nil {
+				println("Error for Delegator Address: ", msg_cancle_unbonding_delegation.DelegatorAddress)
+			}
 		}
 	}
-
-	beginredelegations := am.keeper.Stakingmiddleware.DequeueAllRedelegation(ctx)
-	println("BeginRedelegations: ", beginredelegations)
-	println("BeginRedelegations len: ", len(beginredelegations))
-	for _, beginredelegation := range beginredelegations {
-		println("Delegator Address: ", beginredelegation.DelegatorAddress)
-		println("Validator Address: ", beginredelegation.ValidatorSrcAddress)
-		// fmt.Println("Amount", delegation.Amount.Amount)
-
-		// msgDelegate := stakingtypes.MsgDelegate{DelegatorAddress: delegation.DelegatorAddress, ValidatorAddress: delegation.ValidatorAddress, Amount: delegation.Amount}
-		// _, err := am.msgServer.Delegate(ctx, &msgDelegate)
-		// if err != nil {
-		// 	println("Error for Delegator Address: ", delegation.DelegatorAddress)
-		// }
-	}
-
-	undelegations := am.keeper.Stakingmiddleware.DequeueAllUndelegation(ctx)
-	println("Undelegation: ", beginredelegations)
-	println("Undelegation len: ", len(beginredelegations))
-	for _, undelegation := range undelegations {
-		println("Undelegation Delegator Address: ", undelegation.DelegatorAddress)
-		println("Undelegation Validator Address: ", undelegation.ValidatorAddress)
-		// fmt.Println("Amount", delegation.Amount.Amount)
-
-		// msgDelegate := stakingtypes.MsgDelegate{DelegatorAddress: delegation.DelegatorAddress, ValidatorAddress: delegation.ValidatorAddress, Amount: delegation.Amount}
-		// _, err := am.msgServer.Delegate(ctx, &msgDelegate)
-		// if err != nil {
-		// 	println("Error for Delegator Address: ", delegation.DelegatorAddress)
-		// }
-	}
-
-	//DequeueAllCancelUnbondingDelegation
-	cancel_unbonding_delegations := am.keeper.Stakingmiddleware.DequeueAllCancelUnbondingDelegation(ctx)
-	println("Cancel Unbonding Delegations: ", cancel_unbonding_delegations)
-	println("Cancel Ubonding Delegations len: ", len(cancel_unbonding_delegations))
-	for _, undelegation := range cancel_unbonding_delegations {
-		println("Cancel Unbonding Delegation  Delegator Address: ", undelegation.DelegatorAddress)
-		println("Cancel Unbonding Delegations Validator Address: ", undelegation.ValidatorAddress)
-		// fmt.Println("Amount", delegation.Amount.Amount)
-
-		// msgDelegate := stakingtypes.MsgDelegate{DelegatorAddress: delegation.DelegatorAddress, ValidatorAddress: delegation.ValidatorAddress, Amount: delegation.Amount}
-		// _, err := am.msgServer.Delegate(ctx, &msgDelegate)
-		// if err != nil {
-		// 	println("Error for Delegator Address: ", delegation.DelegatorAddress)
-		// }
-	}
-
-	// ctx.EventManager().EmitEvent(
-	// 	sdk.NewEvent(
-	// 		"DequeueAllDelegation",
-	// 		// sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
-	// 		// sdk.NewAttribute(types.AttributeKeyValidator, dvPair.ValidatorAddress),
-	// 		// sdk.NewAttribute(types.AttributeKeyDelegator, dvPair.DelegatorAddress),
-	// 	),
-	// )
 
 	return am.AppModule.EndBlock(ctx, _abc)
 }
