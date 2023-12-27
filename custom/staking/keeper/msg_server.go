2ff3fe7646b9d187379f6665356ba2d1e5d8e146
rustdev <placex.com@gmail.com>
2023-12-27 23:31:53 +0000

    introduce protection into unbond/rebond staking service tx from abuse

--- custom/staking/keeper/msg_server.go
+++ custom/staking/keeper/msg_server.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 )
@@ -31,11 +32,25 @@ func (k msgServer) Delegate(goCtx context.Context, msg *types.MsgDelegate) (*typ
 }
 
 func (k msgServer) BeginRedelegate(goCtx context.Context, msg *types.MsgBeginRedelegate) (*types.MsgBeginRedelegateResponse, error) {
-	return k.msgServer.BeginRedelegate(goCtx, msg)
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	params := k.Keeper.Stakingmiddleware.GetParams(ctx)
+	height := ctx.BlockHeight()
+	epoch_progress_block_number := (height % int64(params.BlocksPerEpoch))
+	if epoch_progress_block_number > int64(params.AllowUnbondAfterEpochProgressBlockNumber) || epoch_progress_block_number == 0 {
+		return k.msgServer.BeginRedelegate(goCtx, msg)
+	}
+	return &types.MsgBeginRedelegateResponse{}, nil
 }
 
 func (k msgServer) Undelegate(goCtx context.Context, msg *types.MsgUndelegate) (*types.MsgUndelegateResponse, error) {
-	return k.msgServer.Undelegate(goCtx, msg)
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	params := k.Keeper.Stakingmiddleware.GetParams(ctx)
+	height := ctx.BlockHeight()
+	epoch_progress_block_number := (height % int64(params.BlocksPerEpoch))
+	if epoch_progress_block_number > int64(params.AllowUnbondAfterEpochProgressBlockNumber) || epoch_progress_block_number == 0 {
+		return k.msgServer.Undelegate(goCtx, msg)
+	}
+	return &types.MsgUndelegateResponse{}, nil
 }
 
 func (k msgServer) CancelUnbondingDelegation(goCtx context.Context, msg *types.MsgCancelUnbondingDelegation) (*types.MsgCancelUnbondingDelegationResponse, error) {
