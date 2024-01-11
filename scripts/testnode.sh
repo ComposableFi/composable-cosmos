164d1cf332864b2bad451c47c8a10caebc096eb6
rustdev <placex.com@gmail.com>
2024-01-11 16:35:17 +0000

    scripts to test 2 cosmos chains ibc connection locally after cosmwasm

--- scripts/testnode.sh
+++ scripts/testnode.sh
@@ -6,32 +6,33 @@ MONIKER="localtestnet"
 KEYALGO="secp256k1"
 KEYRING="test"
 LOGLEVEL="info"
+BANKSY_HOME=~/.banksy
 # to trace evm
 #TRACE="--trace"
 TRACE=""
 
 # remove existing daemon
-rm -rf ~/.centauri*
+rm -rf ~/.banksy*
 
-centaurid config keyring-backend $KEYRING
-centaurid config chain-id $CHAINID
+layerd config keyring-backend $KEYRING
+layerd config chain-id $CHAINID
 
 # if $KEY exists it should be deleted
-echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | centaurid keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
+echo "decorate bright ozone fork gallery riot bus exhaust worth way bone indoor calm squirrel merry zero scheme cotton until shop any excess stage laundry" | layerd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover --home $BANKSY_HOME
 
-centaurid init $MONIKER --chain-id $CHAINID 
+layerd init $MONIKER --chain-id $CHAINID --home $BANKSY_HOME
 
 # Allocate genesis accounts (cosmos formatted addresses)
-centaurid add-genesis-account $KEY 100000000000000000000000000stake --keyring-backend $KEYRING
+layerd add-genesis-account $KEY 100000000000000000000000000stake --keyring-backend $KEYRING --home $BANKSY_HOME
 
 # Sign genesis transaction
-centaurid gentx $KEY 1000000000000000000000stake --keyring-backend $KEYRING --chain-id $CHAINID
+layerd gentx $KEY 1000000000000000000000stake --keyring-backend $KEYRING --chain-id $CHAINID --home $BANKSY_HOME
 
 # Collect genesis tx
-centaurid collect-gentxs
+layerd collect-gentxs --home $BANKSY_HOME
 
 # Run this to ensure everything worked and that the genesis file is setup correctly
-centaurid validate-genesis
+layerd validate-genesis --home $BANKSY_HOME
 
 if [[ $1 == "pending" ]]; then
   echo "pending mode is on, please wait for the first block committed."
@@ -39,7 +40,8 @@ fi
 
 # update request max size so that we can upload the light client
 # '' -e is a must have params on mac, if use linux please delete before run
-sed -i'' -e 's/max_body_bytes = /max_body_bytes = 1/g' ~/.centauri/config/config.toml
+sed -i'' -e 's/max_body_bytes = /max_body_bytes = 1/g' ~/.banksy/config/config.toml
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-centaurid start --pruning=nothing  --minimum-gas-prices=0.0001stake --rpc.laddr tcp://0.0.0.0:26657
+layerd start --pruning=nothing  --minimum-gas-prices=0.0001stake --rpc.laddr tcp://127.0.0.1:36657 --grpc.address localhost:1090 --p2p.laddr tcp://0.0.0.0:36656 --api.address tcp://localhost:2317 --rpc.pprof_laddr tcp://127.0.0.1:7060 --grpc-web.address localhost:1091 --home $BANKSY_HOME
+# layerd start --pruning=nothing  --minimum-gas-prices=0.0001stake --rpc.laddr tcp://0.0.0.0:26657 --rpc.grpc_laddr tcp://0.0.0.0:9090 --home $BANKSY_HOME
---
+++ scripts/testnodeVlad2.sh
@@ -0,0 +1,54 @@
+KEY="mykey"
+CHAINID="centauri-testnet-2"
+MONIKER="localtestnet"
+KEYALGO="secp256k1"
+KEYRING="test"
+LOGLEVEL="info"
+# to trace evm
+#TRACE="--trace"
+TRACE=""
+BANKSY_HOME=~/.banksy2
+
+# validate dependencies are installed
+command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+
+# remove existing daemon
+rm -rf ~/.banksy2
+alias centaurid=centaurid-nogov-2/centaurid
+
+centaurid config keyring-backend $KEYRING
+centaurid config chain-id $CHAINID
+
+# if $KEY exists it should be deleted
+#echo "taste shoot adapt slow truly grape gift need suggest midnight burger horn whisper hat vast aspect exit scorpion jewel axis great area awful blind" | centaurid keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover
+#echo "sense state fringe stool behind explain area quit ugly affair develop thumb clinic weasel choice atom gesture spare sea renew penalty second upon peace" | centaurid keys add k1 --keyring-backend $KEYRING --algo $KEYALGO --recover
+echo "sense state fringe stool behind explain area quit ugly affair develop thumb clinic weasel choice atom gesture spare sea renew penalty second upon peace" | centaurid keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover --home $BANKSY_HOME
+
+centaurid init $MONIKER --chain-id $CHAINID --home $BANKSY_HOME
+
+# Allocate genesis accounts (centauri formatted addresses)
+centaurid add-genesis-account $KEY 10000000000000000000stake --keyring-backend $KEYRING --home $BANKSY_HOME
+#centaurid add-genesis-account k1 10000000000000000000stake --keyring-backend $KEYRING
+
+# Sign genesis transaction centauri1594tdya20hxz7kjenkn5w09jljyvdfk8kx5rd6
+centaurid gentx $KEY 100000000000000000stake --keyring-backend $KEYRING --chain-id $CHAINID --home $BANKSY_HOME
+
+# Collect genesis tx
+centaurid collect-gentxs --home $BANKSY_HOME
+
+# Run this to ensure everything worked and that the genesis file is setup correctly
+centaurid validate-genesis --home $BANKSY_HOME
+
+if [[ $1 == "pending" ]]; then
+  echo "pending mode is on, please wait for the first block committed."
+fi
+
+# update request max size so that we can upload the light client
+# '' -e is a must have params on mac, if use linux please delete before run
+sed -i'' -e 's/max_body_bytes = /max_body_bytes = 1000/g' ~/.banksy2/config/config.toml
+sed -i'' -e 's/max_tx_bytes = /max_tx_bytes = 1000/g' ~/.banksy2/config/config.toml
+cat $HOME/.banksy2/config/genesis.json | jq '.app_state["gov"]["params"]["voting_period"]="45s"' > $HOME/.banksy2/config/tmp_genesis.json && mv $HOME/.banksy2/config/tmp_genesis.json $HOME/.banksy2/config/genesis.json
+
+# Start the node (remove the --pruning=nothing flag if historical queries are not needed)
+centaurid start --pruning=nothing  --minimum-gas-prices=0stake --rpc.laddr tcp://127.0.0.1:36657 --grpc.address localhost:1090 --p2p.laddr tcp://0.0.0.0:36656 --api.address tcp://localhost:2317 --rpc.pprof_laddr tcp://127.0.0.1:7060 --grpc-web.address localhost:1091 --home $BANKSY_HOME
+# centaurid start --pruning=nothing  --minimum-gas-prices=0stake
