961a9c8aee34f1ef33b8a4782b044d6c241ce2eb
Kostya Kastsevich <kkastsevich@gmail.com>
2024-01-17 16:00:18 +0300

    reward distribution transfer function

--- x/stakingmiddleware/types/keys.go
+++ x/stakingmiddleware/types/keys.go
@@ -2,12 +2,14 @@ package types
 
 // ParamsKey is the key to use for the keeper store.
 var (
-	ParamsKey = []byte{0x01} // key for global staking middleware params in the keeper store
+	ParamsKey      = []byte{0x01} // key for global staking middleware params in the keeper store
+	RewardDenomKey = []byte{0x02}
 )
 
 const (
 	// module name
-	ModuleName = "stakingmiddleware"
+	ModuleName       = "stakingmiddleware"
+	RewardModuleName = "fee_collector"
 
 	// StoreKey is the default store key for stakingmiddleware module that store params when apply validator set changes and when allow to unbond/redelegate
 
