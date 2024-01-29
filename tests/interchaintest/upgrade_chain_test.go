5ddacffdc439164d1411d68b56ffd7d255ddcd3b
Kostya Kastsevich <kkastsevich@gmail.com>
2024-01-29 20:22:28 +0300

    t

--- tests/interchaintest/upgrade_chain_test.go
+++ tests/interchaintest/upgrade_chain_test.go
@@ -20,7 +20,7 @@ const (
 
 func TestCentauriUpgrade(t *testing.T) {
 	repo, version := GetDockerImageInfo()
-	CosmosChainUpgradeTest(t, repo, version, "v7")
+	CosmosChainUpgradeTest(t, repo, version, "v6_4")
 }
 
 func CosmosChainUpgradeTest(t *testing.T, upgradeContainerRepo, upgradeVersion, upgradeName string) {
