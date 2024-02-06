c2dd545d6d3e3e6ef9d7ee659f12110431c38352
kkast <kkastsevich@gmail.com>
2024-02-07 02:42:41 +0300

    delete proposal #6

---
+++ app/upgrades/v6_4_5/constants.go
@@ -0,0 +1,16 @@
+package v6_4_5
+
+import (
+	"github.com/notional-labs/composable/v6/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the composable upgrade.
+	UpgradeName    = "v6_4_5"
+	BrokenProposal = 6
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+}
