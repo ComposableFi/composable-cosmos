98d1a4402b9a34f74939d2db8f9be1ebfa3da5dc
rustdev <placex.com@gmail.com>
2024-02-21 13:40:19 +0000

    custom ibc transfer module ready to replace for testing.

--- custom/ibc-transfer/module.go
+++ custom/ibc-transfer/module.go
@@ -1,58 +1,47 @@
-// package bank
+package bank
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"github.com/cosmos/cosmos-sdk/codec"
-// 	"github.com/cosmos/cosmos-sdk/types/module"
-// 	stakingmodule "github.com/cosmos/cosmos-sdk/x/staking"
-// 	"github.com/cosmos/cosmos-sdk/x/staking/exported"
-// 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
-// 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
 
-// 	// custombankkeeper "github.com/notional-labs/composable/v6/custom/bank/keeper"
+	ibctransfermodule "github.com/cosmos/ibc-go/v7/modules/apps/transfer"
+	ibctransferkeeper "github.com/cosmos/ibc-go/v7/modules/apps/transfer/keeper"
+	"github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	customibctransferkeeper "github.com/notional-labs/composable/v6/custom/ibc-transfer/keeper"
+)
 
-// 	customstakingkeeper "github.com/notional-labs/composable/v6/custom/staking/keeper"
-// )
+// AppModule wraps around the bank module and the bank keeper to return the right total supply
+type AppModule struct {
+	ibctransfermodule.AppModule
+	keeper customibctransferkeeper.Keeper
+	// subspace  exported.Subspace
+	msgServer types.MsgServer
+}
 
-// // AppModule wraps around the bank module and the bank keeper to return the right total supply
-// type AppModule struct {
-// 	stakingmodule.AppModule
-// 	keeper    customstakingkeeper.Keeper
-// 	subspace  exported.Subspace
-// 	msgServer stakingtypes.MsgServer
-// }
+// NewAppModule creates a new AppModule object
+func NewAppModule(cdc codec.Codec, keeper customibctransferkeeper.Keeper) AppModule {
+	stakingModule := ibctransfermodule.NewAppModule(keeper.Keeper)
+	return AppModule{
+		AppModule: stakingModule,
+		keeper:    keeper,
+		// subspace:  ss,
+		msgServer: keeper.Keeper,
+	}
+}
 
-// // NewAppModule creates a new AppModule object
-// func NewAppModule(cdc codec.Codec, keeper customstakingkeeper.Keeper, accountKeeper stakingtypes.AccountKeeper, bankKeeper stakingtypes.BankKeeper, ss exported.Subspace) AppModule {
-// 	stakingModule := stakingmodule.NewAppModule(cdc, &keeper.Keeper, accountKeeper, bankKeeper, ss)
-// 	return AppModule{
-// 		AppModule: stakingModule,
-// 		keeper:    keeper,
-// 		subspace:  ss,
-// 		msgServer: stakingkeeper.NewMsgServerImpl(&keeper.Keeper),
-// 	}
-// }
+// RegisterServices registers module services.
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterMsgServer(cfg.MsgServer(), am.keeper.Keeper)
+	types.RegisterQueryServer(cfg.QueryServer(), am.keeper.Keeper)
 
-// // RegisterServices registers module services.
-// // NOTE: Overriding this method as not doing so will cause a panic
-// // when trying to force this custom keeper into a bankkeeper.BaseKeeper
-// func (am AppModule) RegisterServices(cfg module.Configurator) {
-// 	// types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(&am.keeper))
-// 	stakingtypes.RegisterMsgServer(cfg.MsgServer(), customstakingkeeper.NewMsgServerImpl(am.keeper.Keeper, am.keeper))
-// 	querier := stakingkeeper.Querier{Keeper: &am.keeper.Keeper}
-// 	stakingtypes.RegisterQueryServer(cfg.QueryServer(), querier)
+	m := ibctransferkeeper.NewMigrator(am.keeper.Keeper)
+	if err := cfg.RegisterMigration(types.ModuleName, 1, m.MigrateTraces); err != nil {
+		panic(fmt.Sprintf("failed to migrate transfer app from version 1 to 2: %v", err))
+	}
 
-// 	m := stakingkeeper.NewMigrator(&am.keeper.Keeper, am.subspace)
-// 	if err := cfg.RegisterMigration(stakingtypes.ModuleName, 1, m.Migrate1to2); err != nil {
-// 		panic(fmt.Sprintf("failed to migrate x/staking from version 1 to 2: %v", err))
-// 	}
-
-// 	if err := cfg.RegisterMigration(stakingtypes.ModuleName, 2, m.Migrate2to3); err != nil {
-// 		panic(fmt.Sprintf("failed to migrate x/staking from version 2 to 3: %v", err))
-// 	}
-
-// 	if err := cfg.RegisterMigration(stakingtypes.ModuleName, 3, m.Migrate3to4); err != nil {
-// 		panic(fmt.Sprintf("failed to migrate x/staking from version 3 to 4: %v", err))
-// 	}
-// }
+	if err := cfg.RegisterMigration(types.ModuleName, 2, m.MigrateTotalEscrowForDenom); err != nil {
+		panic(fmt.Sprintf("failed to migrate transfer app from version 2 to 3: %v", err))
+	}
+}
