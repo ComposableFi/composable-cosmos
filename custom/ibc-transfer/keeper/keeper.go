0166205f242ded8ff8cfbb5dbb6aa9ef9487d012
rustdev <placex.com@gmail.com>
2024-02-21 16:39:48 +0000

    add dependency to transfer keeper from ibc transfer middleware/params

--- custom/ibc-transfer/keeper/keeper.go
+++ custom/ibc-transfer/keeper/keeper.go
@@ -9,13 +9,13 @@ import (
 	"github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	porttypes "github.com/cosmos/ibc-go/v7/modules/core/05-port/types"
 	"github.com/cosmos/ibc-go/v7/modules/core/exported"
-	// stakingmiddleware "github.com/notional-labs/composable/v6/x/stakingmiddleware/keeper"
+	ibctransfermiddleware "github.com/notional-labs/composable/v6/x/ibctransfermiddleware/keeper"
 )
 
 type Keeper struct {
 	ibctransferkeeper.Keeper
-	cdc codec.BinaryCodec
-	// Stakingmiddleware *stakingmiddleware.Keeper
+	cdc                   codec.BinaryCodec
+	IbcTransfermiddleware *ibctransfermiddleware.Keeper
 	// authority         string
 }
 
@@ -31,7 +31,7 @@ func NewKeeper(
 	bk types.BankKeeper,
 	scopedKeeper exported.ScopedKeeper,
 	// authority string,
-	// stakingmiddleware *stakingmiddleware.Keeper,
+	ibcTransfermiddleware *ibctransfermiddleware.Keeper,
 	//return type from this function is different from the staking keeper.
 	//todo double check if this is correct
 ) Keeper {
@@ -39,7 +39,8 @@ func NewKeeper(
 		Keeper: ibctransferkeeper.NewKeeper(cdc, key, paramSpace, ics4Wrapper, channelKeeper, portKeeper, authKeeper, bk, scopedKeeper),
 		// authority:         authority,
 		// Stakingmiddleware: stakingmiddleware,
-		cdc: cdc,
+		IbcTransfermiddleware: ibcTransfermiddleware,
+		cdc:                   cdc,
 	}
 	return keeper
 }
