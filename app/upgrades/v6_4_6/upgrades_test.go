51b924e4fa510470e843a31f81f98c2a8c99aeec
tungle <anhletung@notional.ventures>
2024-02-21 16:36:21 +0700

    wip

--- app/upgrades/v6_4_6/upgrades_test.go
+++ app/upgrades/v6_4_6/upgrades_test.go
@@ -2,6 +2,7 @@ package v6_4_6_test
 
 import (
 	"testing"
+	"time"
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -29,7 +30,7 @@ func TestUpgradeTestSuite(t *testing.T) {
 }
 
 // Ensures the test does not error out.
-func (s *UpgradeTestSuite) TestUpgradeModuleGov() {
+func (s *UpgradeTestSuite) TestForMigratingNewPrefix() {
 	// DEFAULT PREFIX: centauri
 	sdk.GetConfig().SetBech32PrefixForAccount(utils.OldBech32PrefixAccAddr, utils.OldBech32PrefixAccPub)
 	sdk.GetConfig().SetBech32PrefixForValidator(utils.OldBech32PrefixValAddr, utils.OldBech32PrefixValPub)
@@ -37,6 +38,25 @@ func (s *UpgradeTestSuite) TestUpgradeModuleGov() {
 
 	s.Setup(s.T())
 
+	acc1, proposal, oldAccBal := prepareForTestingGovModule(s)
+
+	oldConsAddress := prepareForTestingSlashingModule(s)
+
+	oldValAddress, oldValAddress2, acc3, afterOneDay := prepareForTestingStakingModule(s)
+
+	/* == UPGRADE == */
+	upgradeHeight := int64(5)
+	s.ConfirmUpgradeSucceeded(v6_4_6.UpgradeName, upgradeHeight)
+
+	/* == CHECK AFTER UPGRADE == */
+	checkUpgradeGovModule(s, acc1, proposal, oldAccBal)
+	checkUpgradeSlashingModule(s, oldConsAddress)
+	checkUpgradeStakingModule(s, oldValAddress, oldValAddress2, acc3, afterOneDay)
+	// migration of auth has been well tested on above cases
+}
+
+func prepareForTestingGovModule(s *UpgradeTestSuite) (sdk.AccAddress, govtypes.Proposal, sdk.Coin) {
+	/* PREPARE FOR TESTING GOV MODULE */
 	acc1 := s.TestAccs[0]
 
 	// MINT NEW TOKEN FOR BALANCE CHECKING
@@ -62,11 +82,49 @@ func (s *UpgradeTestSuite) TestUpgradeModuleGov() {
 
 	// CHECK BALANCE OF A ACCOUNT
 	oldAccBal := s.App.BankKeeper.GetBalance(s.Ctx, acc1, COIN_DENOM)
+	return acc1, proposal, oldAccBal
+}
 
-	// == UPGRADE ==
-	upgradeHeight := int64(5)
-	s.ConfirmUpgradeSucceeded(v6_4_6.UpgradeName, upgradeHeight)
+func prepareForTestingSlashingModule(s *UpgradeTestSuite) sdk.ConsAddress {
+	/* PREPARE FOR TESTING SLASHING MODULE */
+	acc2 := s.TestAccs[1]
 
+	oldConsAddress, err := utils.ConsAddressFromOldBech32(acc2.String(), utils.OldBech32PrefixAccAddr)
+	s.Suite.Equal(err, nil)
+
+	// CHECK ValidatorSigningInfo
+	s.App.SlashingKeeper.SetValidatorSigningInfo(s.Ctx, oldConsAddress, slashingtypes.ValidatorSigningInfo{
+		Address: oldConsAddress.String(),
+	})
+	return oldConsAddress
+}
+
+func prepareForTestingStakingModule(s *UpgradeTestSuite) (sdk.ValAddress, sdk.ValAddress, sdk.AccAddress, time.Time) {
+	/* PREPARE FOR TESTING SLASHING MODULE */
+	acc3 := s.TestAccs[2]
+
+	// MINT NEW TOKEN FOR BALANCE CHECKING
+	s.App.BankKeeper.MintCoins(s.Ctx, minttypes.ModuleName, sdk.NewCoins(sdk.NewCoin(COIN_DENOM, math.NewIntFromUint64(100000000))))
+	s.App.BankKeeper.SendCoinsFromModuleToAccount(s.Ctx, minttypes.ModuleName, acc3, sdk.NewCoins(sdk.NewCoin(COIN_DENOM, math.NewIntFromUint64(100000000))))
+
+	// validator.OperatorAddress
+	oldValAddress := s.SetupValidator(stakingtypes.Bonded)
+	oldValAddress2 := s.SetupValidator(stakingtypes.Bonded)
+
+	// delegation.DelegatorAddress & delegation.ValidatorAddress
+	s.StakingHelper.Delegate(acc3, oldValAddress, sdk.NewInt(300))
+
+	// redelegation.DelegatorAddress & redelegation.ValidatorSrcAddress & redelegation.ValidatorDstAddress
+	completionTime, err := s.App.StakingKeeper.BeginRedelegation(s.Ctx, acc3, oldValAddress, oldValAddress2, sdk.NewDec(100))
+	afterOneDay := completionTime.AddDate(0, 0, 1)
+	s.Require().NoError(err)
+
+	// Undelegate part of the tokens from val2 (test instant unbonding on undelegation started before upgrade)
+	s.StakingHelper.Undelegate(acc3, oldValAddress, sdk.NewInt(10), true)
+	return oldValAddress, oldValAddress2, acc3, afterOneDay
+}
+
+func checkUpgradeGovModule(s *UpgradeTestSuite, acc1 sdk.AccAddress, proposal govtypes.Proposal, oldAccBal sdk.Coin) {
 	// CONVERT ACC TO NEW PREFIX
 	_, bz, _ := bech32.DecodeAndConvert(acc1.String())
 	newBech32Addr, _ := bech32.ConvertAndEncode(utils.NewBech32PrefixAccAddr, bz)
@@ -95,28 +153,7 @@ func (s *UpgradeTestSuite) TestUpgradeModuleGov() {
 	s.Suite.Equal(deposit.Depositor, newBech32Addr)
 }
 
-func (s *UpgradeTestSuite) TestUpgradeModuleSlashing() {
-	// DEFAULT PREFIX: centauri
-	sdk.GetConfig().SetBech32PrefixForAccount(utils.OldBech32PrefixAccAddr, utils.OldBech32PrefixAccPub)
-	sdk.GetConfig().SetBech32PrefixForValidator(utils.OldBech32PrefixValAddr, utils.OldBech32PrefixValPub)
-	sdk.GetConfig().SetBech32PrefixForConsensusNode(utils.OldBech32PrefixConsAddr, utils.OldBech32PrefixConsPub)
-
-	s.Setup(s.T())
-
-	acc1 := s.TestAccs[0]
-
-	oldConsAddress, err := utils.ConsAddressFromOldBech32(acc1.String(), utils.OldBech32PrefixAccAddr)
-	s.Suite.Equal(err, nil)
-
-	// CHECK ValidatorSigningInfo
-	s.App.SlashingKeeper.SetValidatorSigningInfo(s.Ctx, oldConsAddress, slashingtypes.ValidatorSigningInfo{
-		Address: oldConsAddress.String(),
-	})
-
-	// == UPGRADE ==
-	upgradeHeight := int64(5)
-	s.ConfirmUpgradeSucceeded(v6_4_6.UpgradeName, upgradeHeight)
-
+func checkUpgradeSlashingModule(s *UpgradeTestSuite, oldConsAddress sdk.ConsAddress) {
 	// CONVERT TO ACC TO NEW PREFIX
 	_, bz, _ := bech32.DecodeAndConvert(oldConsAddress.String())
 	newBech32Addr, _ := bech32.ConvertAndEncode(utils.NewBech32PrefixConsAddr, bz)
@@ -128,39 +165,7 @@ func (s *UpgradeTestSuite) TestUpgradeModuleSlashing() {
 	s.Suite.Equal(valSigningInfo.Address, newBech32Addr)
 }
 
-func (s *UpgradeTestSuite) TestUpgradeModuleStaking() {
-	// DEFAULT PREFIX: centauri
-	sdk.GetConfig().SetBech32PrefixForAccount(utils.OldBech32PrefixAccAddr, utils.OldBech32PrefixAccPub)
-	sdk.GetConfig().SetBech32PrefixForValidator(utils.OldBech32PrefixValAddr, utils.OldBech32PrefixValPub)
-	sdk.GetConfig().SetBech32PrefixForConsensusNode(utils.OldBech32PrefixConsAddr, utils.OldBech32PrefixConsPub)
-
-	s.Setup(s.T())
-
-	acc1 := s.TestAccs[0]
-
-	// MINT NEW TOKEN FOR BALANCE CHECKING
-	s.App.BankKeeper.MintCoins(s.Ctx, minttypes.ModuleName, sdk.NewCoins(sdk.NewCoin(COIN_DENOM, math.NewIntFromUint64(100000000))))
-	s.App.BankKeeper.SendCoinsFromModuleToAccount(s.Ctx, minttypes.ModuleName, acc1, sdk.NewCoins(sdk.NewCoin(COIN_DENOM, math.NewIntFromUint64(100000000))))
-
-	// validator.OperatorAddress
-	oldValAddress := s.SetupValidator(stakingtypes.Bonded)
-	oldValAddress2 := s.SetupValidator(stakingtypes.Bonded)
-
-	// delegation.DelegatorAddress & delegation.ValidatorAddress
-	s.StakingHelper.Delegate(acc1, oldValAddress, sdk.NewInt(300))
-
-	// redelegation.DelegatorAddress & redelegation.ValidatorSrcAddress & redelegation.ValidatorDstAddress
-	completionTime, err := s.App.StakingKeeper.BeginRedelegation(s.Ctx, acc1, oldValAddress, oldValAddress2, sdk.NewDec(100))
-	afterOneDay := completionTime.AddDate(0, 0, 1)
-	s.Require().NoError(err)
-
-	// Undelegate part of the tokens from val2 (test instant unbonding on undelegation started before upgrade)
-	s.StakingHelper.Undelegate(acc1, oldValAddress, sdk.NewInt(10), true)
-
-	// == UPGRADE ==
-	upgradeHeight := int64(5)
-	s.ConfirmUpgradeSucceeded(v6_4_6.UpgradeName, upgradeHeight)
-
+func checkUpgradeStakingModule(s *UpgradeTestSuite, oldValAddress sdk.ValAddress, oldValAddress2 sdk.ValAddress, acc1 sdk.AccAddress, afterOneDay time.Time) {
 	// CONVERT TO ACC TO NEW PREFIX
 	_, bz, _ := bech32.DecodeAndConvert(oldValAddress.String())
 	newBech32Addr, _ := bech32.ConvertAndEncode(utils.NewBech32PrefixValAddr, bz)
