161855af4ecb1e8299e3835177d4d2a8490c78c6
kienn6034 <kien@notional.ventures>
2024-02-27 16:31:38 +0700

    chore: log for test upgrade

--- scripts/upgrade/v_6_4_6/pre-script.sh
+++ scripts/upgrade/v_6_4_6/pre-script.sh
@@ -1,24 +1,25 @@
 echo -e "\n ********** Running Pre-Scripts **********"
 
 BINARY=$1 
-DENOM=${2:-pica}
+DENOM=${2:-upica}
 CHAIN_DIR=$(pwd)/mytestnet
 
 KEY="test0"
 KEY1="test1"
 KEY2="test2"
 
-WALLET_1=$($BINARY keys show $KEY1 -a --keyring-backend test --home $CHAIN_DIR)  
-echo "wallet 1: $WALLET_1"
-
 
 DEFAULT_GAS_FLAG="--gas 3000000 --gas-prices 0.025$DENOM --gas-adjustment 1.5"
 DEFAULT_ENV_FLAG="--keyring-backend test --chain-id localpica --home $CHAIN_DIR"
 
+
+
 echo "binary value: $BINARY"
-echo "pwd: $(pwd)"
 COUNTER_CONTRACT_DIR=$(pwd)/scripts/upgrade/contracts/counter.wasm
 
+WALLET_1=$($BINARY keys show $KEY1 -a --keyring-backend test --home $CHAIN_DIR)  
+BALANCE_1=$($BINARY query bank balances $WALLET_1 --home $CHAIN_DIR -o json | jq -r '.balances[0].amount')
+echo "wallet 1: $WALLET_1 - balance: $BALANCE_1"
 
 
 ############ Settingup WASM environment ############
@@ -38,13 +39,16 @@ TX_HASH=$($BINARY tx wasm instantiate2 $CODE_ID '{"count": 0}' $RANDOM_HASH --no
 
 sleep 1
 CONTRACT_ADDRESS=$($BINARY query tx $TX_HASH -o json | jq -r '.logs[0].events[1].attributes[0].value')
-echo "Contract address: $CONTRACT_ADDRESS"
+echo "Contract address deployed at: $CONTRACT_ADDRESS"
 
 ## Execute the contract, increment counter to 1
+echo "wallet1: call the increment() function"
 $BINARY tx wasm execute $CONTRACT_ADDRESS '{"increment":{}}' --from $KEY1 $DEFAULT_ENV_FLAG $DEFAULT_GAS_FLAG -y -o json > /dev/null
 
 ## assert counter value to be 1
 sleep 1
+echo "wallet1: call the get_count() function"
+$BINARY query wasm contract-state smart $CONTRACT_ADDRESS '{"get_count":{"addr": "'"$WALLET_1"'"}}' -o json 
 COUNTER_VALUE=$($BINARY query wasm contract-state smart $CONTRACT_ADDRESS '{"get_count":{"addr": "'"$WALLET_1"'"}}' -o json | jq -r '.data.count')
 if [ "$COUNTER_VALUE" -ne 1 ]; then
     echo "Assertion failed: Expected counter value to be 1, got $COUNTER_VALUE"
