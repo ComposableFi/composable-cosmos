79e03f35a826ddc742050db7012877d00cef7994
dzmitry-lahoda <dzmitry@lahoda.pro>
2024-02-28 12:10:22 +0000

    allow devnet with custom key gov

--- app/app.go
+++ app/app.go
@@ -296,6 +296,7 @@ func NewComposableApp(
 	encodingConfig EncodingConfig,
 	appOpts servertypes.AppOptions,
 	wasmOpts []wasm.Option,
+	devnetGov *string,
 	baseAppOptions ...func(*baseapp.BaseApp),
 ) *ComposableApp {
 	appCodec := encodingConfig.Marshaler
@@ -336,6 +337,7 @@ func NewComposableApp(
 		appOpts,
 		wasmOpts,
 		enabledProposals,
+		devnetGov,
 	)
 
 	// transferModule := transfer.NewAppModule(app.TransferKeeper)
