fcdbe5d6bdd3787199dd2264fdbbdb92c06cbe6d
rustdev <placex.com@gmail.com>
2024-03-07 21:50:35 +0000

    create a new chain upgrade handler for chain upgrade.

---
+++ app/upgrades/v6_4_7/constants.go
@@ -0,0 +1,21 @@
+package v6_4_7
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+
+	"github.com/notional-labs/composable/v6/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the composable upgrade.
+	UpgradeName = "v6_4_6"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
