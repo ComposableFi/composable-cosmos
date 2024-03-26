cee444918dc7e5bcf1b0a3bed02d74645bd509ed
rustdev <placex.com@gmail.com>
2024-03-26 08:55:32 +0000

    v6_4_91 chain upgrade name to replace original ibc module

---
+++ app/upgrades/v6_4_91/constants.go
@@ -0,0 +1,20 @@
+package v6_4_91
+
+import (
+	store "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/notional-labs/composable/v6/app/upgrades"
+)
+
+const (
+	// UpgradeName defines the on-chain upgrade name for the composable upgrade.
+	UpgradeName = "v6_4_91"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName:          UpgradeName,
+	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: store.StoreUpgrades{
+		Added:   []string{},
+		Deleted: []string{},
+	},
+}
